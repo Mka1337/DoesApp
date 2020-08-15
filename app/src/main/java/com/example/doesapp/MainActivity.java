d9fba9451dd64ae86f6e1d687bbf4b7f2c495fcf
mka1337 <moroz.kirill.99@gmail.com>
2020-08-15 13:32:42 +0300

    first commit

---
+++ app/src/main/java/com/example/doesapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.doesapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference reference;
+    RecyclerView ourdoes;
+    ArrayList<MyDoes> list;
+    DoesAdapter doesAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ourdoes = findViewById(R.id.ourdoes);
+        ourdoes.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<MyDoes>();
+
+        //get data from firebase
+        reference = FirebaseDatabase.getInstance().getReference().child("DoesApp");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //set code retrive data and replace layout
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+                {
+                    MyDoes p = dataSnapshot1.getValue(MyDoes.class);
+                    list.add(p);
+                }
+                doesAdapter = new DoesAdapter(MainActivity.this, list);
+                ourdoes.setAdapter(doesAdapter);
+                doesAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //set code an error
+                Toast.makeText(getApplicationContext(),"No data", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
